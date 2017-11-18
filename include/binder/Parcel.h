f0620f03ddfbd4f72481c5d0aac0e694ed91d0f6
Michael Wachenschwanz <mwachens@google.com>
2017-11-17 18:25:05 -0800

    Disallow reading object data from Parcels with non-object reads
    The check added to each non-object reads adds an overhead. If the
    objects (binders and file descriptors) were written to the Parcel in
    sequential order then check adds a small O(1) overhead to each read,
    plus an O(N) overhead to the first read (to verify the N objects were
    added in order).
    If the objects were written out of order (as in by jumping around the
    Parcel
    with setDataPosition and writing Binder, DON'T DO THIS!!) (writing non
    objects out of order is fine), the first read is forced to sort the
    objects
    in the internal bookkeeping. Based on the assumption non sequential
    writes
    are infrequent and overall Parcels are probably mostly sorted, insertion
    sort was used. Worst case sorts will add an O(N^2) overhead to the first
    non object read from the Parcel.
    
    Test: run cts -m CtsOsTestCases -t android.os.cts.ParcelTest
    
    Bug: 29833520
    Change-Id: I82de8eb5f5eb56f869542d5358e96884c24301b2
    (cherry picked from commit c517681c66a1a387be657e0cf06da8d19659dd14)

--- include/binder/Parcel.h
+++ include/binder/Parcel.h
@@ -39,6 +39,7 @@ class ProcessState;
 class String8;
 class TextOutput;
 
+typedef __u32 binder_size_t;
 struct flat_binder_object;  // defined in support_p/binder_module.h
 
 class Parcel {
@@ -234,6 +235,7 @@ private:
     void                freeDataNoInit();
     void                initState();
     void                scanForFds() const;
+    status_t            validateReadData(size_t len) const;
                         
     template<class T>
     status_t            readAligned(T *pArg) const;
@@ -252,6 +254,7 @@ private:
     size_t              mObjectsSize;
     size_t              mObjectsCapacity;
     mutable size_t      mNextObjectHint;
+    mutable bool        mObjectsSorted;
 
     mutable bool        mFdsKnown;
     mutable bool        mHasFds;
